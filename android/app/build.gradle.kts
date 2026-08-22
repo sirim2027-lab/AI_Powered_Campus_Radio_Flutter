eff26c696b14f637ca967d51889930b6aeb6edf7
Mohammed Junaid <junaid@MacBook-Neo.local>
2026-08-22 18:45:22 +0530

    Fix Android package configuration and update Google Services credentials to resolve startup blank screen

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.my_first_app"
+    namespace = "com.mycompany.cognitivetuition"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -19,7 +19,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.my_first_app"
+        applicationId = "com.mycompany.cognitivetuition"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/mycompany/cognitivetuition/MainActivity.kt
+++ android/app/src/main/kotlin/com/mycompany/cognitivetuition/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.my_first_app
+package com.mycompany.cognitivetuition
 
 import io.flutter.embedding.android.FlutterActivity
 
